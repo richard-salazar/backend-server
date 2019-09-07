013e4839f118810f3cdf6851daf9324e18ad1b3e
Richard Salazar <richard.sal.suarez@gmail.com>
2019-09-07 16:24:06 -0500

    usuario y token listo

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 //Requires
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 
 //Inicializar Variables
 var app = express();
 
+
+//Body Parser
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+//Importar rutas
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
+
 //Conexión a la base de datos
-mongoose.connection.openUri('mongodb://localhost:27017/posweb', (err, res) => {
+mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) => {
 
     if(err) throw err;
 
@@ -15,14 +27,10 @@ mongoose.connection.openUri('mongodb://localhost:27017/posweb', (err, res) => {
 })
 
 //Rutas
-app.get('/', (req, res, next) => {
+app.use('/usuario', usuarioRoutes);
+app.use('/login', loginRoutes);
+app.use('/', appRoutes);
 
-    res.status(200).json({
-        ok: true,
-        mensaje: 'Petición realizada correctamente'
-    });
-
-});
 
 //Escuchar peticiones
 app.listen(3000, () => {
